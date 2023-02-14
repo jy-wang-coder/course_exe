bf714770d819d063cceadc28871f57035e9898f6
wangjiayun <jiayun.vv@gmail.com>
2023-02-14 10:41:24 +0800

    part1 exe

--- src/App.js
+++ src/App.js
@@ -1,64 +1,72 @@
+import { useState } from 'react'
+
+const Button = (props) =>(
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = (props) =>{
+  const{text,value} = props
+  return(
+  <p> {text} {value} </p>
+  )
+}
+
+const Statistic = (props) =>{
+  const {good, bad, neutral, total} = props
+  if (total === 0) {
+    return (
+    <div> 
+      <h1> statistics </h1>
+      No feedback is given yet. 
+    </div>
+    )
+  }
+  return(
+    <div>
+      <h1> statistics </h1>
+      <StatisticLine text="good" value ={good} />
+      <StatisticLine text="neutral" value ={neutral} />
+      <StatisticLine text="bad" value ={bad} />
+      <StatisticLine text="total" value ={total} />
+    </div>
+  )
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercise: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercise: 7
-      },
-      {
-        name: 'State of a component',
-        exercise: 14
-      },
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
+
+  const goodhandleClick = () =>{
+    console.log('good before', good)
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const neutralhandleClick = () =>{
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const badhandleClick = () =>{
+    setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return (
     <div>
-      <Head course={course}/>
-      <Content course={course} />
-      <Total course={course}/>
+      <h1> give feedback </h1>
+      <Button handleClick={goodhandleClick} text='good'/>
+      <Button handleClick={neutralhandleClick} text='neutral'/>
+      <Button handleClick={badhandleClick} text='bad'/>
+      <Statistic good={good} bad={bad} neutral={neutral} total={total}/>
     </div>
   )
 }
-
-const Head = (props) => {
-  console.log(props)
-  return <h1>{props.course.name}</h1>
-}
-
-const Content = (props) =>{
-  return (
-    <div>
-      <p>
-        {props.course.parts[0].name} {props.course.parts[0].exercise}
-      </p>
-      <p>
-        {props.course.parts[1].name} {props.course.parts[1].exercise}
-      </p>
-      <p>
-        {props.course.parts[2].name} {props.course.parts[2].exercise}
-      </p>
-    </div>
-  )
-}
-
-
-
-const Total = (props) =>{
-  return (
-    <div>
-      <p>Number of exercises {props.course.parts[0].exercise + props.course.parts[1].exercise + props.course.parts[2].exercise}</p>
-    </div>
-  )
-}
-
-
 
 export default App
